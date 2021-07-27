f1218f29d48dbbba65f9169263050fa283522b93
sarvp <41444785+sarvp@users.noreply.github.com>
2021-07-27 13:27:50 +0530

    Add files via upload

--- Src/main.c
+++ Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "UartRingbuffer_multi.h"
+
 #include "stdio.h"
 #include "String.h"
 #include"stdbool.h"
@@ -36,9 +36,6 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define MSG_LENGTH 1500
-
-#define device_uart &huart3
-#define pc_uart &huart2
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -49,10 +46,8 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
-UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
-//char rxbuffer[100], txbuffer[100];
 char rxgsm[100] = {0} ;
 char rx2[100] = {0} ;
 char buf[1000] = {0};
@@ -63,23 +58,149 @@ char message[MSG_LENGTH] = {0};
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_USART3_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void gsm_Send(const char *);
-void SerialPrint(const char * serialstring);
-bool gsm_Check_with_cmd(const char *sendCMD, const char *cmpCMD);
-bool gsm_init(void);
-bool is_gsm_registred(void);
-bool is_sms_unread(void);
-
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void SerialPrint(const char *serialstring)
+{
+	char sendCMD[100] = {0};
+	sprintf(sendCMD,serialstring);
+	HAL_UART_Transmit(&huart2, (uint8_t *) sendCMD, strlen(sendCMD), HAL_MAX_DELAY);
+}
+
+/*
+void SerialPrintln(const char serialstringln)
+{
+	//sprintf(sendCMD,gsmCMD);
+	//const char nl = '\n';
+	//const char serialstring = (const char)serialstringln + nl;
+	//HAL_UART_Transmit(&huart2, (uint8_t *) serialstring, sizeof(serialstring), HAL_MAX_DELAY);
+}
+*/
+
+void gsm_Send(const char *gsmCMD)
+{
+	char sendCMD[300] = {0};
+	sprintf(sendCMD,gsmCMD);
+	HAL_UART_Transmit(&huart1, (uint8_t *) sendCMD, strlen(sendCMD), HAL_MAX_DELAY);
+	memset(buf,0,sizeof(buf));
+	HAL_UART_Receive(&huart1, (uint8_t *)buf, sizeof(buf),500);
+}
+
+bool gsm_Check_with_cmd(const char *sendCMD, const char *cmpCMD)
+{
+	//memset(buf,0,sizeof(buf));
+	gsm_Send(sendCMD);
+	HAL_Delay(100);
+	if( strstr((char *)buf,cmpCMD) )
+	{
+		return true;
+	}
+	else return false ;
+}
+
+bool gsm_init(void)
+{
+	if(!gsm_Check_with_cmd("AT\r\n", "OK"))
+	{
+		memset(buf,0,sizeof(buf));
+		return false ;
+	}
+	memset(buf,0,sizeof(buf));
+	if(!gsm_Check_with_cmd("AT+CFUN=1\r\n", "OK"))
+	{
+		memset(buf,0,sizeof(buf));
+		return false ;
+	}
+	memset(buf,0,sizeof(buf));
+	if(!gsm_Check_with_cmd("AT+CMGF=1\r\n", "OK"))
+	{
+		memset(buf,0,sizeof(buf));
+		return false ;
+	}
+	memset(buf,0,sizeof(buf));
+	if(!gsm_Check_with_cmd("AT+CNMI=1,1,0,0,0\r\n", "OK"))
+	{
+		memset(buf,0,sizeof(buf));
+		return false ;
+	}
+	memset(buf,0,sizeof(buf));
+	if(!gsm_Check_with_cmd("AT+CPIN?\r\n", "+CPIN: READY"))
+	{
+		memset(buf,0,sizeof(buf));
+		return false ;
+	}
+	//memset(buf,0,sizeof(buf));
+	return true ;
+}
+
+bool is_gsm_registred(void)
+{
+	if(!gsm_Check_with_cmd("AT+CREG?\r\n", "+CREG: 1,1"))
+	{
+		memset(buf,0,sizeof(buf));
+		gsm_Send("AT+CREG=1\r\r\n");
+		HAL_Delay(2000);
+		memset(buf,0,sizeof(buf));
+		if(!gsm_Check_with_cmd("AT+CREG?\r\n", "+CREG: 1,1"))
+		{
+			return false ;
+		}
+	}
+	return true ;
+}
+
+bool is_sms_unread(void)
+{
+	//gsm_check_with_cmd("AT+CMGL=\"REC UNREAD\",1");
+	char sendCMD[100], smsrxcheck[30] = {0};
+	//sprintf(sendCMD,"AT+CMGL=\"REC UNREAD\",1\n");
+	//HAL_UART_Transmit(&huart1, (uint8_t *) sendCMD, strlen(sendCMD), 100);
+	gsm_Send("AT+CMGL=\"REC UNREAD\",1\r\n");
+	memset(smsrxcheck,0,sizeof(smsrxcheck));
+	sprintf(smsrxcheck,buf);
+	//SerialPrint(smsrxcheck);
+	//HAL_UART_Receive(&huart1, (uint8_t *)message, MSG_LENGTH, 5000);
+	if( !strstr(smsrxcheck,"OK") )
+	{
+		char smsdata[200] = {0};
+		memset(sendCMD,0,sizeof(sendCMD));
+		//sprintf(sendCMD,"AT+CMGL=\"REC UNREAD\",1\n");
+		//HAL_UART_Transmit(&huart1, (uint8_t *) sendCMD, strlen(sendCMD), 100);
+		gsm_Send("AT+CMGL=\"REC UNREAD\",1\r\n");
+		//memset(smsrxcheck,0,sizeof(smsrxcheck));
+		memset(smsdata,0,sizeof(smsdata));
+		sprintf(smsdata,buf);
+		//HAL_UART_Receive(&huart1, (uint8_t *)smsdata, (80+MSG_LENGTH), 5000);
+		//SerialPrint(smsdata);
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+/*
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	HAL_UART_Receive_IT(&huart1, (uint8_t *)buf, 100);
+}
+void flushstr(char *flushstr)
+{
+	char emptystr[sizeof(*flushstr)];
+	for(int sizecount = 0; sizecount < sizeof(*flushstr); sizecount++)
+	{
+		emptystr[sizecount] = '\0';
+	}
+	sprintf(flushstr,emptystr);
+}
+*/
 /* USER CODE END 0 */
 
 /**
@@ -89,8 +210,12 @@ bool is_sms_unread(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	//char serialstring[100];
-	//uint8_t count = 0;
+
+	uint8_t i = 0 ;
+
+    char msg[100] = {0};
+    //char cmd[100] = {0};
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -111,53 +236,203 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART1_UART_Init();
   MX_USART2_UART_Init();
-  MX_USART3_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  Ringbuf_init();
-  int i = 0;
-  char c[6] ="AT\r\n";
+  /*
+  	sprintf(msg,"SMS with GSM Module\r\n");
+  	HAL_UART_Transmit(&huart2, msg,sizeof(msg),1000);
+  	sprintf(msg,"Checking Module...\r\n");
+  	HAL_UART_Transmit(&huart2, msg,sizeof(msg),1000);
+  	uint8_t flag=1;
+
+  	while(flag){
+  		sprintf(cmd,"AT\r\n");
+  		HAL_UART_Transmit(&huart1,cmd,sizeof(cmd),1000);
+  		HAL_UART_Receive_IT (&huart1, buf, 30);
+  		HAL_Delay(1000);
+
+  		sprintf(msg,buf);
+  		HAL_UART_Transmit(&huart2,msg,sizeof(msg),1000);
+
+  		if(strstr((char *)buf,"OK")){
+  			sprintf(msg,"Module Connected\r\n");
+  			HAL_UART_Transmit(&huart2,msg,sizeof(msg),1000);
+  			flag=0;
+  		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+
+  		}
+  		HAL_Delay(1000);
+  		HAL_UART_Transmit(&huart2,buf,sizeof(buf),1000);
+  		memset(buf,0,sizeof(buf));
+  	}
+
+  	sprintf(msg,"Sending Message\r\n");
+  	HAL_UART_Transmit(&huart2,msg,sizeof(msg),1000);
+  	sprintf(cmd,"AT+CMGF=1\r\n");
+  	HAL_UART_Transmit(&huart1,cmd,sizeof(cmd),1000);
+  	HAL_UART_Receive_IT (&huart1, buf, 30);
+  	HAL_Delay(1000);
+  	HAL_UART_Transmit(&huart2,buf,sizeof(buf),1000);
+  	memset(buf,0,sizeof(buf));
+
+  	sprintf(cmd,"AT+CMGS=\"%s\"\r\n",mobile);
+  	HAL_UART_Transmit(&huart1, cmd,sizeof(cmd),1000);
+  	HAL_Delay(100);
+  	sprintf(cmd,"Hello from GSM%c",0x1a);
+  	HAL_UART_Transmit(&huart1, cmd,sizeof(cmd),1000);
+  	//HAL_UART_Transmit(&huart1, ((char)(0x1a)),1,1000);
+  	HAL_UART_Receive_IT (&huart1, buf, 30);
+  	HAL_Delay(5000);
+
+  	HAL_UART_Transmit(&huart2, buf,sizeof(buf),1000);
+  	sprintf(msg,"Message Sent\r\n");
+  	HAL_UART_Transmit(&huart2, msg,sizeof(msg),1000);*/
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+    while (1)
+    {
+    	//sprintf(msg,"In While Loop :\n");
+    	//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)sizeof(msg),HAL_MAX_DELAY);
+    	//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
+    	//HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11|GPIO_PIN_9, RESET);
+    	//uint8_t status = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+    	if( ! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5))
+    	{
+    		while(! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5));
+    		//sprintf(msg,"Switch Pressed...\n");
+    		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t )sizeof(msg),HAL_MAX_DELAY);
+    		sprintf(msg,"Switch 1 Pressed\n");
+    		SerialPrint(msg);
+    		memset(msg,0,sizeof(msg));
+    		//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_11|GPIO_PIN_9);
+    		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, RESET);
+    		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
+    		//memset(msg,0,sizeof(msg));
+    		i ++ ;
+    		sprintf(msg,"%d\n",i);
+    		SerialPrint(msg);
+    		memset(msg,0,sizeof(msg));
+    		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)sizeof(msg),HAL_MAX_DELAY);
+      		//sprintf(cmd,"ATD 8668780789;\r\n");
+    		//sprintf(cmd,"AT+CREG?\r\n");
+      		//HAL_UART_Transmit(&huart1,(uint8_t *)cmd,(uint16_t)sizeof(cmd),HAL_MAX_DELAY);
+    		//sprintf(msg,"AT+CREG?\r\n");
+    		//bool id = gsm_Check_with_cmd(msg, "+CREG: 1,1");
+    		//sprintf(msg,"AT+CREG?\r\n");
+    		//gsm_Send(msg);
+    		//memset(msg,0,sizeof(msg));
+    		//bool id = gsm_Check_with_cmd("AT\r\n","OK");
+    		//HAL_UART_Receive(&huart1, (uint8_t *)buf, 100, 1000);
+      		//HAL_Delay(100);
+    		//bool id = gsm_init();
+      		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)strlen(msg),HAL_MAX_DELAY);
+      		//if(strstr((char *)buf,"+CREG: 1,1")){
+      		//if(is_gsm_registred()){
+      		if(gsm_init())	{
+    			sprintf(msg,(const char *)buf);
+      			SerialPrint(msg);
+      			//sprintf(msg,"Calling . . .\r\n");
+      			//sprintf(msg,"Registered on Netwrok . . .\r\n");
+      			SerialPrint("Registered on Netwrok . . .\r\n");
+      			memset(msg,0,sizeof(msg));
+      			//HAL_UART_Transmit(&huart2,(uint8_t *)buf,(uint16_t)sizeof(buf),HAL_MAX_DELAY);
+      			//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+      			//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9|GPIO_PIN_11);
+      			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, SET);
+      			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
+      			HAL_Delay(300);
+      			//memset(buf,0,sizeof(buf));
+      		}
+      		else HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
+    	}
+    	memset(msg,0,sizeof(msg));
+    	memset(buf,0,sizeof(buf));
 
+    	if( ! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4))
+    	{
+    		while(! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4));
+    		//sprintf(msg,"Switch Pressed...\n");
+    		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t )sizeof(msg),HAL_MAX_DELAY);
+    		sprintf(msg,"Switch 2 Pressed\n");
+    		SerialPrint(msg);
+    		memset(msg,0,sizeof(msg));
+    		//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_11|GPIO_PIN_9);
+    		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, RESET);
+    		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
+    		//memset(msg,0,sizeof(msg));
+    		i ++ ;
+    		sprintf(msg,"%d\n",i);
+    		SerialPrint(msg);
+    		memset(msg,0,sizeof(msg));
+    		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)sizeof(msg),HAL_MAX_DELAY);
+    		//sprintf(cmd,"ATD 8668780789;\r\n");
+    		//sprintf(cmd,"AT+CREG?\r\n");
+    		//HAL_UART_Transmit(&huart1,(uint8_t *)cmd,(uint16_t)sizeof(cmd),HAL_MAX_DELAY);
+    		//sprintf(msg,"AT\r\n");
+    		//gsm_Send(msg);
+    		//memset(msg,0,sizeof(msg));
+    		//bool id = gsm_Check_with_cmd("AT\r\n","OK");
+    		//HAL_UART_Receive(&huart1, (uint8_t *)buf, 100,1000);
+    		//HAL_Delay(100);
+
+    		//sprintf(msg,(const char *)buf);
+    		//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)strlen(msg),HAL_MAX_DELAY);
+    		//if(strstr((char *)buf,"OK\r\n")){
+    		if(is_gsm_registred()){
+    			sprintf(msg,(const char *)buf);
+    			SerialPrint(msg);
+    			sprintf(msg,"Initialized . . .\r\n");
+    			//sprintf(msg,"New Message Received . . .\r\n");
+    			SerialPrint(msg);
+    			memset(msg,0,sizeof(msg));
+    			//HAL_UART_Transmit(&huart2,(uint8_t *)buf,(uint16_t)sizeof(buf),HAL_MAX_DELAY);
+    			//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    			//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9|GPIO_PIN_11);
+    			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, SET);
+    			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
+    			HAL_Delay(300);
+    			//memset(buf,0,sizeof(buf));
+    		}
+    		else HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
+    	}
+    	memset(msg,0,sizeof(msg));
+    	memset(buf,0,sizeof(buf));
+    	//HAL_UART_Receive(&huart1, (uint8_t *)buf, 100, 1000);
+    	//HAL_Delay(100);
+    	//sprintf(msg,(const char *)buf);
+    	//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)sizeof(msg),HAL_MAX_DELAY);
+    	//memset(msg,0,sizeof(msg));
+    	//memset(buf,0,sizeof(buf));
+    	//sprintf(msg,"In While Loop, Switch not pressed...\n");
+    	//HAL_UART_Transmit(&huart2,(uint8_t *)msg,(uint16_t)sizeof(msg),HAL_MAX_DELAY);
+    	//else ;
+      //HAL_UART_Receive_IT (&huart1, buf, 100);
+      //HAL_UART_Receive(&huart1, buf, sizeof(buf), HAL_MAX_DELAY);
+      //HAL_Delay(300);
+      //sprintf(msg,buf);
+      //HAL_UART_Transmit(&huart2,msg,sizeof(msg),HAL_MAX_DELAY);
+      //memset(buf,0,sizeof(buf));
+      //memset(cmd,0,sizeof(cmd));
+      //memset(msg,0,sizeof(msg));
+      //HAL_UART_Receive_IT (&huart2, buf, 100);
+      //HAL_UART_Receive(&huart2, buf, sizeof(buf), HAL_MAX_DELAY);
+      //sprintf(cmd,buf);
+      //HAL_UART_Transmit(&huart1,cmd,sizeof(cmd),HAL_MAX_DELAY);
+      //HAL_Delay(1000);
+      //HAL_UART_Transmit(&huart2,buf,sizeof(buf),1000);
+      //memset(buf,0,sizeof(buf));
+      //memset(cmd,0,sizeof(cmd));
+      //memset(msg,0,sizeof(msg));
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  if (IsDataAvailable(pc_uart))
-	  {
-		  int data = Uart_read(pc_uart);
-		  Uart_write(data, device_uart);
-	  }
-
-	  if (IsDataAvailable(device_uart))
-	  {
-		  int data = Uart_read(device_uart);
-		  Uart_write(data, pc_uart);
-	  }
-
-	  /*if( ! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5))
-	  {
-		  HAL_Delay(200);
-		  while(! HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5));
-		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
-		  gsm_Send("AT\r\n");
-	  }
-
-	  while(IsDataAvailable())
-	  {
-		  int data = Uart_read();
-		  SerialPrint(data);
-		  //SerialPrint((char *)data);
-	  }
-	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
-	  */
-  }
+    }
   /* USER CODE END 3 */
 }
 
@@ -215,7 +490,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
+  huart1.Init.BaudRate = 4800;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
@@ -266,39 +541,6 @@ static void MX_USART2_UART_Init(void)
 }
 
 /**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART3_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 9600;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
-}
-
-/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -314,10 +556,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -332,8 +574,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  /*Configure GPIO pins : PB1 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -342,108 +584,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-/*
-void SerialPrint(int serialstring)
-{
-	char sendCMD;
-	sendCMD = (char)serialstring;
-	//sprintf(sendCMD,serialstring);
-	HAL_UART_Transmit(&huart2, (uint8_t *) sendCMD, sizeof(sendCMD), HAL_MAX_DELAY);
-}
-*/
-void SerialPrint(const char *serialstring)
-{
-	char sendCMD[100] = {0};
-	sprintf(sendCMD,serialstring);
-	HAL_UART_Transmit(&huart2, (uint8_t *) sendCMD, strlen(sendCMD), HAL_MAX_DELAY);
-}
 
-/*
-void SerialPrintln(const char serialstringln)
-{
-	//sprintf(sendCMD,gsmCMD);
-	//const char nl = '\n';
-	//const char serialstring = (const char)serialstringln + nl;
-	//HAL_UART_Transmit(&huart2, (uint8_t *) serialstring, sizeof(serialstring), HAL_MAX_DELAY);
-}
-*/
-
-void gsm_Send(const char *gsmCMD)
-{
-	char sendCMD[300] = {0};
-	sprintf(sendCMD,gsmCMD);
-	int sendcnt = 0;
-	while( !(sendcnt == strlen(sendCMD)) )
-	{
-		Uart_write((int)sendCMD[sendcnt], device_uart);
-		sendcnt ++ ;
-	}
-	//HAL_UART_Transmit(&huart1, (uint8_t *) sendCMD, strlen(sendCMD), HAL_MAX_DELAY);
-	//memset(buf,0,sizeof(buf));
-	//HAL_UART_Receive_IT(&huart1, (uint8_t *)buf, sizeof(buf));
-}
-
-bool gsm_Check_with_cmd(const char *sendCMD, const char *cmpCMD)
-{
-	//memset(buf,0,sizeof(buf));
-	gsm_Send(sendCMD);
-	HAL_Delay(100);
-	if( strstr((char *)buf,cmpCMD) )
-	{
-		return true;
-	}
-	else return false ;
-}
-
-bool gsm_init(void)
-{
-	if(!gsm_Check_with_cmd("AT\r\n", "OK"))
-	{
-		memset(buf,0,sizeof(buf));
-		return false ;
-	}
-	memset(buf,0,sizeof(buf));
-	/*if(!gsm_Check_with_cmd("AT+CFUN=1\r\n", "OK"))
-	{
-		memset(buf,0,sizeof(buf));
-		return false ;
-	}
-	memset(buf,0,sizeof(buf));
-	if(!gsm_Check_with_cmd("AT+CMGF=1\r\n", "OK"))
-	{
-		memset(buf,0,sizeof(buf));
-		return false ;
-	}
-	memset(buf,0,sizeof(buf));*/
-	if(!gsm_Check_with_cmd("AT+CNMI=1,1,0,0,0\r\n", "OK"))
-	{
-		memset(buf,0,sizeof(buf));
-		return false ;
-	}
-	memset(buf,0,sizeof(buf));
-	if(!gsm_Check_with_cmd("AT+CPIN?\r\n", "+CPIN: READY"))
-	{
-		memset(buf,0,sizeof(buf));
-		return false ;
-	}
-	memset(buf,0,sizeof(buf));
-	return true ;
-}
-
-bool is_gsm_registred(void)
-{
-	if(!gsm_Check_with_cmd("AT+CREG?\r\n", "+CREG: 1,1"))
-	{
-		memset(buf,0,sizeof(buf));
-		gsm_Send("AT+CREG=1\r\n");
-		memset(buf,0,sizeof(buf));
-		if(!gsm_Check_with_cmd("AT+CREG?\r\n", "+CREG: 1,1"))
-		{
-			return false ;
-		}
-	}
-	return true ;
-}
 /* USER CODE END 4 */
 
 /**
--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -53,14 +53,11 @@
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-extern void Uart_isr (UART_HandleTypeDef *huart);
-
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart2;
-extern UART_HandleTypeDef huart3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -209,9 +206,9 @@ void SysTick_Handler(void)
 void USART1_IRQHandler(void)
 {
   /* USER CODE BEGIN USART1_IRQn 0 */
-	Uart_isr (&huart1);
+
   /* USER CODE END USART1_IRQn 0 */
-  //HAL_UART_IRQHandler(&huart1);
+  HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
 
   /* USER CODE END USART1_IRQn 1 */
@@ -223,26 +220,12 @@ void USART1_IRQHandler(void)
 void USART2_IRQHandler(void)
 {
   /* USER CODE BEGIN USART2_IRQn 0 */
-	Uart_isr(&huart2);
+
   /* USER CODE END USART2_IRQn 0 */
-  //HAL_UART_IRQHandler(&huart2);
+  HAL_UART_IRQHandler(&huart2);
   /* USER CODE BEGIN USART2_IRQn 1 */
 
   /* USER CODE END USART2_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USART3 global interrupt.
-  */
-void USART3_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART3_IRQn 0 */
-	Uart_isr(&huart3);
-  /* USER CODE END USART3_IRQn 0 */
-  //HAL_UART_IRQHandler(&huart3);
-  /* USER CODE BEGIN USART3_IRQn 1 */
-
-  /* USER CODE END USART3_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
